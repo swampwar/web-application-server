24a1de084fdbb6bc445fcf4bb59000c31e341d78
Yang <Yang@175.208.219.183>
2017-07-18 23:11:35 +0900

    첫번째 커밋

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -17,9 +17,10 @@ public class WebServer {
         } else {
             port = Integer.parseInt(args[0]);
         }
+        
+        System.out.println("변경점!");
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
 
